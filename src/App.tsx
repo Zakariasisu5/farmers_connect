5283dcff3f4af6ffd06f712211bf45229bafca04
Zakariasisu5 <zakariasisu5@gmail.com>
2025-11-20 00:09:35 +0000

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
 
 import AuthProvider from "./contexts/AuthContext";
@@ -34,7 +34,7 @@ const App = () => (
           <TooltipProvider>
             <Toaster />
             <Sonner />
-            <BrowserRouter>
+            <HashRouter>
             <Routes>
               {/* Public Routes */}
               <Route path="/" element={<Landing />} />
@@ -110,7 +110,7 @@ const App = () => (
               {/* 404 Route */}
               <Route path="*" element={<NotFound />} />
             </Routes>
-            </BrowserRouter>
+            </HashRouter>
           </TooltipProvider>
         </LanguageProvider>
       </AuthProvider>
